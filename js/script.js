e8bddfccb64afcd0f2bbc57b3c50b2c9220beece
0adev <ayoubaguirar44@gmail.com>
2024-06-11 18:14:21 +0100

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,90 +1,90 @@
+// Cache selectors
 const primaryHeader = document.querySelector(".primary-header");
 const navLinks = document.querySelector(".nav-links");
 const menuList = document.querySelector(".menu-list");
 const featuresImgFrameLarge = document.querySelector(".features .image");
 const featuresIconLarge = document.querySelector(".features .feature-icon");
-const featuresImgFrameSmall = [
-  document.querySelector(".first-feature img:first-of-type"),
-  document.querySelector(".second-feature img:first-of-type"),
-  document.querySelector(".third-feature img:first-of-type"),
-];
-const featuresIconSmall = [
-  document.querySelector(".first-feature .icon"),
-  document.querySelector(".second-feature .icon"),
-  document.querySelector(".third-feature .icon"),
-];
+const featuresImgFrameSmall = document.querySelectorAll(
+  ".feature img:first-of-type"
+);
+const featuresIconSmall = document.querySelectorAll(".feature .icon");
 
-// toggle menu
+// Toggle menu
 const handleNavClick = (e) => {
   const target = e.target;
   const parentClassList = target.parentElement.classList;
-  const targetClassList = target.classList;
 
   if (parentClassList.contains("hamburger-menu")) {
     e.preventDefault();
     menuList.classList.add("open-menu");
-    return;
-  } else if (targetClassList.contains("close")) {
+  } else if (target.classList.contains("close")) {
     e.preventDefault();
     menuList.classList.remove("open-menu");
-    return;
   }
 };
 
-// features section
-
+// Handle scroll
 const handleWindowScroll = () => {
   const scrollY = window.scrollY;
   const screenwidth = window.innerWidth;
-  let largeScreenMin = 800;
+  const largeScreenMin = 800;
   const largeScreenMax = 1524;
-  let smallScreenMax = 796;
+  const smallScreenMax = 796;
 
-  const featuresSection = () => {
-    const largeScreen = () => {
-      if (scrollY > 2150) {
-        featuresImgFrameLarge.style.backgroundImage =
-          "url(./assets/hero-img1.jpg)";
-        featuresIconLarge.style.backgroundImage =
-          "url(./assets/perfume-svgrepo-com.svg)";
-      } else if (scrollY > 1508) {
-        featuresImgFrameLarge.style.backgroundImage =
-          "url(./assets/features-img02.jpg)";
-        featuresIconLarge.style.backgroundImage =
-          "url(./assets/quality-medal-.svg)";
-      } else {
-        featuresImgFrameLarge.style.backgroundImage =
-          "url(./assets/features-img01.jpg)";
-        featuresIconLarge.style.backgroundImage =
-          "url(./assets/timer-forward-.svg)";
-      }
-    };
-
-    const smallScreen = () => {
-      const scrollY = window.scrollY;
-
-      if (scrollY > 770) {
-        featuresImgFrameSmall[0].classList.add("translateX-0");
-        featuresIconSmall[0].classList.add("translateX-0");
-      }
-      if (scrollY > 1410) {
-        featuresImgFrameSmall[1].classList.add("translateX-0");
-        featuresIconSmall[1].classList.add("translateX-0");
-      }
-      if (scrollY > 2010) {
-        featuresImgFrameSmall[2].classList.add("translateX-0");
-        featuresIconSmall[2].classList.add("translateX-0");
-      }
-    };
-
-    if (screenwidth <= largeScreenMax && screenwidth >= largeScreenMin) {
-      largeScreen();
-    } else if (screenwidth <= smallScreenMax) {
-      smallScreen();
+  const updateLargeScreen = () => {
+    if (scrollY > 2150) {
+      featuresImgFrameLarge.style.backgroundImage =
+        "url(./assets/hero-img1.jpg)";
+      featuresIconLarge.style.backgroundImage =
+        "url(./assets/perfume-svgrepo-com.svg)";
+    } else if (scrollY > 1508) {
+      featuresImgFrameLarge.style.backgroundImage =
+        "url(./assets/features-img02.jpg)";
+      featuresIconLarge.style.backgroundImage =
+        "url(./assets/quality-medal-.svg)";
+    } else {
+      featuresImgFrameLarge.style.backgroundImage =
+        "url(./assets/features-img01.jpg)";
+      featuresIconLarge.style.backgroundImage =
+        "url(./assets/timer-forward-.svg)";
     }
   };
 
-  featuresSection();
+  const updateSmallScreen = () => {
+    const addClasses = (index) => {
+      if (index >= 0 && index < featuresImgFrameSmall.length) {
+        featuresImgFrameSmall[index].classList.add("translateX-0");
+      }
+      if (index >= 0 && index < featuresIconSmall.length) {
+        featuresIconSmall[index].classList.add("translateX-0");
+      }
+    };
+
+    const removeClasses = () => {
+      featuresImgFrameSmall.forEach((image) => {
+        image.classList.remove("translateX-0");
+      });
+      featuresIconSmall.forEach((icon) => {
+        icon.classList.remove("translateX-0");
+      });
+    };
+
+    if (scrollY > 2010) {
+      addClasses(2);
+    } else if (scrollY > 1410) {
+      addClasses(1);
+    } else if (scrollY > 770) {
+      addClasses(0);
+    } else {
+      removeClasses();
+    }
+  };
+
+  if (screenwidth <= largeScreenMax && screenwidth >= largeScreenMin) {
+    updateLargeScreen();
+  } else if (screenwidth <= smallScreenMax) {
+    updateSmallScreen();
+  }
 };
 
 // Event Listeners
